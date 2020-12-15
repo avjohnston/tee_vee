3ad04ee5c044e324d7a5e5ebaf1af9b82f76a46e
Andrew Johnston <av.johnst@gmail.com>
2020-12-15 09:35:39 -0700

    FInish Iteration 2

---
+++ lib/character.rb
@@ -0,0 +1,13 @@
+class Character
+  attr_reader :name,
+              :actor,
+              :salary
+
+  def initialize(character)
+    @name = character[:name]
+    @actor = character[:actor]
+    @salary = character[:salary]
+  end
+
+
+end
